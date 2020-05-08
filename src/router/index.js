c4c1ea274e4224f2f9def32c4fa6d9143570feff
André Lagacione <lagacioneandre@gmail.com>
2020-05-08 16:37:09 -0300

    add login route

--- src/router/index.js
+++ src/router/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/products', require('../product'));
 router.use('/create-user', require('../create-user'));
+router.use('/login', require('../login'));
 
 module.exports = router;
